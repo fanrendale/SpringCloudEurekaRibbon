a01da283e775e275610f8374e7ee85a830b409f7
fanrendale <1053314919@qq.com>
2019-04-02 15:32:50 +0800

    服务的消费者加入熔断机制

--- src/main/java/com/xjf/eurekaribbon/EurekaRibbonApplication.java
+++ src/main/java/com/xjf/eurekaribbon/EurekaRibbonApplication.java
@@ -3,11 +3,15 @@ package com.xjf.eurekaribbon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 /**
  * 启用Eureka服务发现，相关配置
  * @author Administrator
  */
+//启用Hystrix熔断机制相关配置
+@EnableHystrix
+//启用Eureka服务发现相关配置
 @EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaRibbonApplication {
